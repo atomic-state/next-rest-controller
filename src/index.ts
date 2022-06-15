c09cfb55ec68a4b28f456f1c32b7d89ab71bb1bb
danybeltran <josuebeltran_2010@hotmail.com>
2022-06-14 23:15:43 -0600

    FEAT: Add express-like support for params (or query)

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,12 @@ export function Controller(path: string, paths: ControllerMethods = {}) {
         let finalQuery: any = {}
 
         handleParts.forEach((handlePart, i) => {
-          if (handlePart.startsWith("[") && handlePart.endsWith("]")) {
+          if (
+            (handlePart.startsWith("[") && handlePart.endsWith("]")) ||
+            handlePart.startsWith(":")
+          ) {
             hasHandlers = true
-            const withoutBrackets = handlePart.replace(/\[|\]/g, "")
+            const withoutBrackets = handlePart.replace(/\[|\]|\:/g, "")
 
             finalQuery[withoutBrackets] = urlParts[i]
 
